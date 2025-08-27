55b9ea5eb8c4868acb0587789d4a231c4da6c575
Cody Karcher <cody.karcher@gmail.com>
2025-08-26 21:52:07 -0700

    Update ESP and Anaconda installation scripts
    Updated download links and installation scripts for ESP and Anaconda to version 2025.06. Removed deprecated Ipopt installation steps.

--- MacOS/mac_setup.sh
+++ MacOS/mac_setup.sh
@@ -13,11 +13,11 @@ cd /Users/$USER/software
 cd /Users/$USER/software
 
 if [ $(arch) = "arm64" ]; then
-    curl -O https://acdl.mit.edu/ESP/PreBuilts/ESP126-macos-arm64.tgz
-    tar -xvzf ESP126-macos-arm64.tgz
+    curl -O https://acdl.mit.edu/ESP/PreBuilts/ESP128-macos-arm64.tgz
+    tar -xvzf ESP128-macos-arm64.tgz
 else
-    curl -O https://acdl.mit.edu/ESP/PreBuilts/ESP126-macos-x86_64.tgz
-    tar -xvzf ESP126-macos-x86_64.tgz
+    curl -O https://acdl.mit.edu/ESP/PreBuilts/ESP128-macos-x86_64.tgz
+    tar -xvzf ESP128-macos-x86_64.tgz
 fi
 
 export PATH="/opt/homebrew/bin:$PATH"
@@ -33,15 +33,15 @@ brew install openmpi
 
 cd /Users/$USER/software
 if [ $(arch) = "arm64" ]; then
-    curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-MacOSX-arm64.sh
-    bash /Users/$USER/software/Anaconda3-2024.10-1-MacOSX-arm64.sh -p /Users/$USER/software/anaconda3
+    curl -O https://repo.anaconda.com/archive/Anaconda3-2025.06-1-MacOSX-arm64.sh
+    bash /Users/$USER/software/Anaconda3-2025.06-1-MacOSX-arm64.sh -p /Users/$USER/software/anaconda3
     cd /Users/$USER/software
-    rm -rf Anaconda3-2024.10-1-MacOSX-arm64.sh
+    rm -rf Anaconda3-2025.06-1-MacOSX-arm64.sh
 else
-    curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-MacOSX-x86_64.sh 
-    bash /Users/$USER/software/Anaconda3-2024.10-1-MacOSX-x86_64.sh -p /Users/$USER/software/anaconda3
+    curl -O https://repo.anaconda.com/archive/Anaconda3-2025.06-1-MacOSX-x86_64.sh 
+    bash /Users/$USER/software/Anaconda3-2025.06-1-MacOSX-x86_64.sh -p /Users/$USER/software/anaconda3
     cd /Users/$USER/software
-    rm -rf Anaconda3-2024.10-1-MacOSX-x86_64.sh
+    rm -rf Anaconda3-2025.06-1-MacOSX-x86_64.sh
 fi
 
 rm -f /Users/$USER/.zshenv
@@ -51,52 +51,13 @@ source /Users/$USER/.zshenv
 export PATH="/Users/$USER/software/anaconda3/bin:$PATH"
 # Corrects PYTHONPATH environment variable
 #export PYTHONPATH=""
-export PYTHONPATH="/Users/$USER/software/anaconda3/lib/python3.12/lib-dynload:$PYTHONPATH"
+export PYTHONPATH="/Users/$USER/software/anaconda3/lib/python3.13/lib-dynload:$PYTHONPATH"
 export PYTHONPATH="/Users/$USER/software/anaconda3/bin:$PYTHONPATH"
 export PYTHONPATH="/Users/$USER/software/anaconda3/lib:$PYTHONPATH"
-export PYTHONPATH="/Users/$USER/software/anaconda3/lib/python3.12/site-packages:$PYTHONPATH"
+export PYTHONPATH="/Users/$USER/software/anaconda3/lib/python3.13/site-packages:$PYTHONPATH"
 
 conda update --all
-conda install -c conda-forge cvxopt
 pip install pint
-
-mkdir /Users/$USER/software/ipopt
-cd /Users/$USER/software/ipopt
-git clone https://github.com/coin-or/Ipopt.git
-git clone https://github.com/coin-or-tools/ThirdParty-ASL.git
-git clone https://github.com/coin-or-tools/ThirdParty-HSL.git
-
-cd /Users/$USER/software/ipopt/ThirdParty-ASL
-./get.ASL
-
-mkdir /Users/$USER/software/ipopt/ASL_build
-cd /Users/$USER/software/ipopt/ThirdParty-ASL
-./configure --prefix=/Users/$USER/software/ipopt/ASL_build
-make install
-
-cd /Users/$USER/software/ipopt/ThirdParty-HSL
-mkdir coinhsl
-cp -a /Users/$USER/software/MA27/ma27-1.0.0/. /Users/$USER/software/ipopt/ThirdParty-HSL/coinhsl
-mv /Users/$USER/software/ipopt/ThirdParty-HSL/coinhsl/src /Users/$USER/software/ipopt/ThirdParty-HSL/coinhsl/ma27
-
-cd /Users/$USER/software/ipopt
-mkdir HSL_build
-cd /Users/$USER/software/ipopt/ThirdParty-HSL
-./configure --prefix=/Users/$USER/software/ipopt/HSL_build
-make install
-
-cd /Users/$USER/software/ipopt/Ipopt
-mkdir /Users/$USER/software/ipopt/build
-./configure --prefix=/Users/$USER/software/ipopt/build  --with-asl-lflags="-L/Users/$USER/software/ipopt/ASL_build/lib -lcoinasl" --with-hsl-lflags="-L/Users/$USER/software/ipopt/HSL_build/lib -lcoinhsl" --disable-java --with-asl-cflags="-I/Users/$USER/software/ipopt/ASL_build/include/coin-or/asl" --with-hsl-cflags="-I/Users/$USER/software/ipopt/HSL_build/include/coin-or/hsl"
-
-make
-make test
-make install
-
-rm -f /Users/$USER/.zshenv
-cp /Users/$USER/software/mae451/MacOS/zshenv_2.txt /Users/$USER/.zshenv
-source /Users/$USER/.zshenv
-
 
 cd /Users/$USER/software/
 pip install -e mae451
