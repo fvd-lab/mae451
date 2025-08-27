6546c1e5840960f7b111bbe79d5d2b2742723c47
Cody Karcher <cody.karcher@gmail.com>
2025-08-27 12:17:27 -0700

    Update PATH for ESP128 in bash profile script

--- Windows/create_bash_profile_2.sh
+++ Windows/create_bash_profile_2.sh
@@ -1,7 +1,7 @@
 touch /home/$USER/bash_profile_2w.txt
 
 printf 'echo "Hello $USER, Running .bash_profile" \n' >> /home/$USER/bash_profile_2w.txt
-printf 'export PATH="/home/$USER/software/ESP126/EngSketchPad/bin:$PATH" \n' >> /home/$USER/bash_profile_2w.txt
+printf 'export PATH="/home/$USER/software/ESP128/EngSketchPad/bin:$PATH" \n' >> /home/$USER/bash_profile_2w.txt
 printf 'export PATH="/home/$USER/software/anaconda3/bin:$PATH" \n' >> /home/$USER/bash_profile_2w.txt
 printf 'export PYTHONPATH="/home/$USER/software/anaconda3/lib/python3.12/lib-dynload:$PYTHONPATH "\n' >> /home/$USER/bash_profile_2w.txt
 printf 'export PYTHONPATH="/home/$USER/software/anaconda3/bin:$PYTHONPATH "\n' >> /home/$USER/bash_profile_2w.txt
